59621dcb88865cde18969fa598dacf84b3a48f82
pkt-ps <shdyh2003@gmail.com>
2017-11-20 12:00:34 +0900

    updated.

--- init.bat
+++ init.bat
@@ -1,25 +1,17 @@
-copy .\_gvimrc %userprofile%\_gvimrc /Y
-cd /d %userprofile%
-rmdir /S /Q .\vimfiles
-mkdir .\vimfiles
-mkdir .\vimfiles\pack
-mkdir .\vimfiles\pack\colors
-mkdir .\vimfiles\pack\my
-mkdir .\vimfiles\pack\my\opt
-mkdir .\vimfiles\pack\my\start
-cd /d %userprofile%\vimfiles
-git clone https://github.com/tomasr/molokai.git
-move .\molokai\colors .\colors
-cd /d %userprofile%\vimfiles\pack\my\start
-REM git clone https://github.com/scrooloose/nerdtree.git
-REM git clone https://github.com/jistr/vim-nerdtree-tabs.git
-REM git clone https://github.com/nathanaelkane/vim-indent-guides.git
-git clone https://github.com/thinca/vim-singleton.git
-git clone https://github.com/itchyny/vim-highlighturl.git
+@echo off
 
-REM unite
-git clone https://github.com/Shougo/unite.vim.git
-git clone https://github.com/Shougo/neomru.vim.git
-git clone https://github.com/Shougo/neoyank.vim.git
+cd %~dp0
+rem 差分が発生して面倒くさいので廃止
+rem copy .\_gvimrc %userprofile%\_gvimrc /Y 
+
+rem シンボリックリンクだとダメ(mklinkで作るっぽい？)
+rem mklink %userprofile%\_gvimrc .\_gvimrc
+
+rem ハードリンクする
+del %userprofile%\_gvimrc
+fsutil hardlink create %userprofile%\_gvimrc .\_gvimrc
+
+rem プラグインのcloneとか配置とか
+call plugin_init.bat
 
 pause
--- plugin_init.bat
+++ plugin_init.bat
@@ -6,15 +6,18 @@ mkdir .\vimfiles\pack\colors
 mkdir .\vimfiles\pack\my
 mkdir .\vimfiles\pack\my\opt
 mkdir .\vimfiles\pack\my\start
+
 cd /d %userprofile%\vimfiles
 git clone https://github.com/tomasr/molokai.git
 move .\molokai\colors .\colors
+
 cd /d %userprofile%\vimfiles\pack\my\start
 git clone https://github.com/scrooloose/nerdtree.git
 rem git clone https://github.com/jistr/vim-nerdtree-tabs.git
 git clone https://github.com/nathanaelkane/vim-indent-guides.git
 git clone https://github.com/thinca/vim-singleton.git
 git clone https://github.com/itchyny/vim-highlighturl.git
+git clone https://github.com/mopp/next-alter.vim
 
 REM unite
 git clone https://github.com/Shougo/unite.vim.git

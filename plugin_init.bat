9138380225900ff08da3929a48d2de810d7708ec
pkt-ps <shdyh2003@gmail.com>
2017-12-11 20:33:26 +0900

    modify

--- plugin_init.bat
+++ plugin_init.bat
@@ -7,10 +7,23 @@ mkdir .\vimfiles\pack\my
 mkdir .\vimfiles\pack\my\opt
 mkdir .\vimfiles\pack\my\start
 
+rem --------------------------------------------
+rem Color Scheme
+rem --------------------------------------------
 cd /d %userprofile%\vimfiles
 git clone https://github.com/tomasr/molokai.git
-move .\molokai\colors .\colors
+move .\molokai\colors\molokai.vim .\colors
+git clone https://github.com/vim-scripts/hybrid.vim.git
+move .\hybrid.vim\colors\hybrid.vim .\colors
 
+rem --------------------------------------------
+rem Syntax
+rem --------------------------------------------
+cd /d %userprofile%\vimfiles\pack\my\start
+
+rem --------------------------------------------
+rem Plugin
+rem --------------------------------------------
 cd /d %userprofile%\vimfiles\pack\my\start
 git clone https://github.com/scrooloose/nerdtree.git
 rem git clone https://github.com/jistr/vim-nerdtree-tabs.git
@@ -19,7 +32,9 @@ git clone https://github.com/thinca/vim-singleton.git
 git clone https://github.com/itchyny/vim-highlighturl.git
 git clone https://github.com/mopp/next-alter.vim
 
-REM unite
+rem --------------------------------------------
+rem Plugin Unite
+rem --------------------------------------------
 git clone https://github.com/Shougo/unite.vim.git
 git clone https://github.com/Shougo/neomru.vim.git
 git clone https://github.com/Shougo/neoyank.vim.git
